b3fd5bc7e84423181ba0d62e7018757e3f385b61
WenHeBule <shixun926@gmail.com>
2026-06-07 18:52:13 +0800

    新增菜品代码实现和完善

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -26,9 +26,9 @@ public interface EmployeeMapper {
      * 插入员工数据
      * @param employee
      */
-//    @Insert("insert into employee(name ,username,password,phone,sex,id_number,create_time,update_time,create_user,update_user)"+
-//               "values"+
-//            "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser})")
+//  @Insert("insert into employee(name ,username,password,phone,sex,id_number,create_time,update_time,create_user,update_user)"+
+ //             "values"+
+  //         "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser})")
 
     @AutoFill(value = OperationType.INSERT)
     void insert(Employee employee);
